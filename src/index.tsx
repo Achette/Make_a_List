f3eb9e649d5f9e6109cb4e09909622f2832ed5d1
Achette <i.achette@live.com>
2023-08-27 16:05:31 -0300

    feat: adiciona provider de tema o projeto

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,14 @@
-import App from "./App";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
-import "./index.css";
+import App from './App'
+import React from 'react'
+import { theme } from './theme/theme'
+import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
-);
+)
