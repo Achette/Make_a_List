b6917ea4a8780d2c25739c7a20f2b1d562a1d07b
Achette <i.achette@live.com>
2023-08-27 16:06:29 -0300

    chore: inicia desenvolvimento do Drawer

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,30 +1,40 @@
 import React from 'react'
+import { Logo } from '../Logo'
 import { HamburgerIcon } from '@chakra-ui/icons'
 import {
   Box,
   IconButton,
   useDisclosure,
   Drawer,
-  DrawerOverlay,
+  DrawerHeader,
   DrawerContent,
+  DrawerCloseButton,
 } from '@chakra-ui/react'
 
 export const LateralDrawew = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
     <>
-      <Box>
+      <Box w="fit-content">
         <IconButton
+          bg="transparent"
           aria-label="hamburguer"
           onClick={onOpen}
           icon={<HamburgerIcon />}
         />
 
-        <Drawer isOpen={isOpen} onClose={onClose} placement="left">
-          <DrawerOverlay />
-
+        <Drawer
+          isOpen={isOpen}
+          onClose={onClose}
+          placement="left"
+          variant="primary"
+        >
           <DrawerContent>
-            DRAWER TESTE
+            <DrawerCloseButton color="white.400" />
+            <DrawerHeader display="flex" mt={8}>
+              <Logo />
+            </DrawerHeader>
           </DrawerContent>
         </Drawer>
       </Box>
