0cdf1596bcf03a5b5eb47ba7e2d66d5dccc17756
Achette <i.achette@live.com>
2023-08-27 16:04:33 -0300

    feat: adiciona persolaização ao tema default

---
+++ src/theme/theme.ts
@@ -0,0 +1,29 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  config: {
+    initialColorMode: 'ligth',
+  },
+  colors: {
+    blue: {
+      50: '#7DC4E4',
+      400: '#029ECE',
+      900: '#27488F',
+    },
+    white: {
+        400: '#D9D9E3'
+    }
+  },
+  components: {
+    Drawer: {
+      parts: ['dialog', 'header', 'body'],
+      variants: {
+        primary: {
+          dialog: {
+            background: 'blue.900',
+          },
+        },
+      },
+    },
+  },
+})
