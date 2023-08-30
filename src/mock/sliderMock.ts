0203d62f04fab5e2f2e5d844902897ae1bee6209
Achette <i.achette@live.com>
2023-08-29 23:36:15 -0300

    feat: cria mock para componente

---
+++ src/mock/sliderMock.ts
@@ -0,0 +1,17 @@
+export const slider = [
+  {
+    title: 'Compartilhe suas listas',
+    content:
+      'Convide amigos e familiares para listas compartilhadas e permita que todos adicionem os itens necessários.',
+  },
+  {
+    title: 'Listas recorrentes',
+    content:
+      'Marque listas como recorrentes e economize tempo com compras diárias ou semanais.',
+  },
+  {
+    title: 'Mais detalhes',
+    content:
+      'Crie listas de compras detalhadas, incluindo produtos, preços e locais de compra',
+  },
+]
