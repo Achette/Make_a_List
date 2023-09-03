e3a92c99a83696733f1d8bc8e6efeb0971a1d117
Achette <i.achette@live.com>
2023-09-03 15:30:02 -0300

    feat: cria página de Sign-in

--- src/routes/Account/Signin/index.tsx
+++ src/routes/Account/Signin/index.tsx
@@ -1,10 +1,79 @@
 import React from 'react'
-import { Box } from "@chakra-ui/react"
-
+import { Link } from 'react-router-dom'
+import { Button, Heading, Input, Text, VStack } from '@chakra-ui/react'
 
 export const SignIn = () => {
+  return (
+    <VStack>
+      <Heading
+        as="h2"
+        color="white.400"
+        w="14rem"
+        textAlign="center"
+        fontSize="2.125rem"
+        fontWeight={700}
+        lineHeight="2.5rem"
+        letterSpacing="0.4px"
+      >
+        Crie uma nova conta
+      </Heading>
 
-    return (
-        <Box>Esta é a página para novo usuário</Box>
-    )
+      <Input
+        w="21.5rem"
+        h="3rem"
+        bg="linear-gradient(0deg, rgba(217, 217, 227, 0.40) 0%, rgba(217, 217, 227, 0.40) 100%), var(--dark-blue, #27488F)"
+        border="none"
+        color="whiteAlpha.900"
+        placeholder="Nome"
+        _placeholder={{ color: 'blue.900' }}
+      />
+
+      <Input
+        w="21.5rem"
+        h="3rem"
+        bg="linear-gradient(0deg, rgba(217, 217, 227, 0.40) 0%, rgba(217, 217, 227, 0.40) 100%), var(--dark-blue, #27488F)"
+        border="none"
+        placeholder="E-mail"
+        _placeholder={{ color: 'blue.900' }}
+      />
+
+      <Input
+        w="21.5rem"
+        h="3rem"
+        bg="linear-gradient(0deg, rgba(217, 217, 227, 0.40) 0%, rgba(217, 217, 227, 0.40) 100%), var(--dark-blue, #27488F)"
+        border="none"
+        placeholder="Senha"
+        _placeholder={{ color: 'blue.900' }}
+        mb="0.75rem"
+      />
+
+      <Link to="#">
+        <Button
+          w="21.5rem"
+          h="3rem"
+          borderRadius="0.625rem"
+          color="blue.900"
+          fontSize="md"
+          fontWeight={500}
+          lineHeight="1.375rem"
+          letterSpacing="-0.41px"
+          
+        >
+          Criar Conta
+        </Button>       
+      </Link>
+
+      <Text
+          m="2.5rem 0 7.75rem 0"
+          textAlign="center"
+          color="white.400"
+          fontSize="md"
+          fontWeight={400}
+          lineHeight="1.375rem"
+          letterSpacing="-0.41px"
+        >
+          Já possui uma conta? <Link to="#">Entrar</Link>
+        </Text>
+    </VStack>
+  )
 }
