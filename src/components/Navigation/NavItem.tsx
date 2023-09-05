9f839097daa80feed2ca8fe4b89dc7c34ed9c34f
Achette <i.achette@live.com>
2023-09-05 10:54:41 -0300

    feat: cria componente NavItem

---
+++ src/components/Navigation/NavItem.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { IconType } from 'react-icons/lib'
+import { useMedia } from '../../hooks/useMedia'
+import { Box, Link as LinkChakra, ListIcon, Text } from '@chakra-ui/react'
+
+type NavItemProps = {
+  item: {
+    type: string
+    label: string
+    icon: IconType
+    path: string
+  }
+  isActive: boolean
+  collapse: boolean
+}
+
+export const NavItem = ({ item, isActive, collapse }: NavItemProps) => {
+  const { icon, label } = item
+
+  const {isDesktop} = useMedia()
+
+  return (
+    <Box display="flex" alignItems="center" my={6} justifyContent="center">
+      <LinkChakra
+        href=""
+        as={Link}
+        gap={4}
+        display="flex"
+        alignItems="center"
+        _hover={{ textDecoration: 'none', color: 'whiteAlpha.900' }}
+        fontWeight="medium"
+        color={isActive ? 'whiteAlpha.900' : 'white.400'}
+        w="full"
+        justifyContent={!collapse ? 'center' : ''}
+      >
+        <ListIcon mt={isDesktop ? "8rem" : 0} as={icon} color="white" w={8} h={8} m="0" />
+        {collapse && <Text fontSize="xl">{label}</Text>}
+      </LinkChakra>
+    </Box>
+  )
+}
