e67aa20e13e21241d2ef60a3bb346d6a0bc65944
Achette <i.achette@live.com>
2023-09-05 10:54:12 -0300

    feat: cria componente Sidebar

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { VStack } from '@chakra-ui/react'
+import { Navigation } from '../Navigation'
+
+export type SidebarProps = {
+  collapse: boolean
+}
+
+export const Sidebar = ({ collapse }: SidebarProps) => {
+  return (
+    <VStack w="full" h="100%" justifyContent="space-between">
+      <Navigation collapse={collapse} />
+    </VStack>
+  )
+}
