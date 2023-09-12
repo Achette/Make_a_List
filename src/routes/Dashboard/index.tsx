a8d389019283dcbc407c7868120f3bf07d039744
Achette <i.achette@live.com>
2023-09-12 19:19:46 -0300

    feat/tag-pi3-23: adiciona comentário

--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -64,7 +64,7 @@ export const Dashboard = () => {
         flexDirection="row"
         position="relative"
         borderRadius="0.5rem"
-        border="1px solid orange"
+        border="1px solid orange" // não esquecer de remover esta linha
       >
         <Outlet />
       </Flex>
