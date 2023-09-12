e38d0ba17c6834808f2528e591273ff1645b0322
Achette <i.achette@live.com>
2023-09-12 16:24:15 -0300

    chore: desenvolve página de Lists

--- src/routes/Dashboard/Lists/index.tsx
+++ src/routes/Dashboard/Lists/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { useMedia } from 'hooks'
 import { SearchBar } from 'components'
+import { AddButton } from 'components/AddButton'
+import { Box, Flex, Text } from '@chakra-ui/react'
 
 export const Lists = () => {
+  const { isMobile } = useMedia()
+
   return (
-    <>
+    <Box w="full" h="full">
       <SearchBar />
-    </>
+      <Flex h="calc(100% - 3.5rem)" alignItems="center" justifyContent="center">
+        <Text
+          fontSize={isMobile ? '0.75rem' : '1rem'}
+          fontWeight={500}
+          color="blue.50"
+        >
+          Sem Listas
+        </Text>
+        <AddButton />
+      </Flex>
+    </Box>
   )
 }
