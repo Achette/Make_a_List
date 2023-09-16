82fb222186d30895dea6819cdf2c96b3c165e87d
Achette <i.achette@live.com>
2023-09-16 17:15:28 -0300

    feat/pi3-32: adiciona componente Lists

--- src/routes/Dashboard/Lists/index.tsx
+++ src/routes/Dashboard/Lists/index.tsx
@@ -1,14 +1,39 @@
 import React from 'react'
 import { useMedia } from 'hooks'
+import { IconType } from 'react-icons/lib'
 import { Box, Flex, Text } from '@chakra-ui/react'
-import { AddButton, SearchBar, List, ListProps } from 'components'
+import { SearchBar, List, AddButton } from 'components'
 
 import { listMock } from 'mock/listmock' // será removido quando conectado ao backend
+
+export type ListsProps = {
+  id: number
+  productLists: {
+    id: number
+    icon: IconType
+    name: string
+    bgColor: string
+    total: number
+    concluded: boolean
+    delete: boolean
+    products: {
+      category: string
+      products: {
+        id: number
+        productName: string
+        place: string
+        price: number
+        quantity: number
+      }[]
+    }[]
+    shared: string[]
+  }[]
+}
 
 export const Lists = () => {
   const { isMobile } = useMedia()
 
-  const [lists, setLists] = React.useState<ListProps[]>([])
+  const [lists, setLists] = React.useState<ListsProps>()
 
   React.useEffect(() => {
     setLists(listMock)
@@ -20,23 +45,25 @@ export const Lists = () => {
       <Flex
         h="calc(100% - 3.5rem)"
         alignItems="center"
-        justifyContent={lists.length ? '' : 'center'}
+        justifyContent={lists?.productLists.length ? '' : 'center'}
         flexDir="column"
         gap="0.75rem"
         py="1.87rem"
         px={isMobile ? '0' : '4rem'}
       >
         {lists &&
-          lists.map((item) => (
+          lists.productLists.map((item, index) => (
             <List
-              key={item.id}
+              key={index}
               bgColor={item.bgColor}
               icon={item.icon}
-              listName={item.listName}
+              listName={item.name}
               shared={item.shared}
+              id={item.id - 1}
             />
           ))}
-        {!lists.length && (
+
+        {!lists?.productLists.length && (
           <Text
             fontSize={isMobile ? '0.75rem' : '1rem'}
             fontWeight={500}
