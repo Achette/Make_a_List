705b37fe7d8753cd1ee2d710a6590d458eba0a5f
Achette <i.achette@live.com>
2023-09-19 09:24:59 -0300

    chore/pi3-38: inicia desenvolvimento dos services para requisição ao backend

--- src/services/auth-services.ts
+++ src/services/auth-services.ts
@@ -1,5 +1,15 @@
+import axios from 'axios'
+import { NewUserProps } from 'routes'
+import { BASE_URL } from 'constants/system'
 import * as accessTokenRepository from 'hooks'
 
+export const UserApi = {
+  newUser: async (data: NewUserProps) => {
+    const response = await axios.post(`${BASE_URL}/singup`, data )
+    return response
+  },
+}
+
 export const getAccessToken = () => {
-    return accessTokenRepository.getToken()
-  }
+  return accessTokenRepository.getToken()
+}
