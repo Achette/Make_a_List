57782a808fc8ed16813298154970d06eb2bb21b2
Achette <i.achette@live.com>
2023-09-19 09:22:23 -0300

    feat/pi3-38: adiciona variáveis de ambiente

--- src/constants/system.ts
+++ src/constants/system.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://make-a-list-back.onrender.com'
+export const BASE_URL = 'https://make-a-list-back-7gvk.onrender.com'
 
 export const TOKEN_KEY = 'com.dev.make_a_list/Token'
