b9287ab7f2096f2643a864da5718b0c2a52ef027
Achette <i.achette@live.com>
2023-09-19 09:25:10 -0300

    chore/pi3-38: inicia desenvolvimento dos services para requisição ao backend

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,14 +1,7 @@
 import { BASE_URL } from 'constants/system'
-import { getAccessToken } from './auth-services'
+
 import axios, { AxiosRequestConfig } from 'axios'
 
 export const requestBackend = (config: AxiosRequestConfig) => {
-  const headers = config.withCredentials
-    ? {
-        ...config.headers,
-        Authorization: 'Bearer ' + getAccessToken(),
-      }
-    : config.headers
-
-  return axios({ ...config, baseURL: BASE_URL, headers })
+  return axios({ ...config, baseURL: BASE_URL })
 }
