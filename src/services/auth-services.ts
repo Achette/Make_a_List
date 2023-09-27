c0b798523b28d995435bdfc16f18d951812e361d
Achette <i.achette@live.com>
2023-09-27 11:47:11 -0300

    feat/pi3-38: cria services para chamada à api

--- src/services/auth-services.ts
+++ src/services/auth-services.ts
@@ -1,12 +1,16 @@
 import axios from 'axios'
-import { NewUserProps } from 'routes'
 import { BASE_URL } from 'constants/system'
 import * as accessTokenRepository from 'hooks'
+import { NewUserProps, UserProps } from 'routes'
 
 export const UserApi = {
   newUser: async (data: NewUserProps) => {
-    const response = await axios.post(`${BASE_URL}/singup`, data )
-    return response
+    const response = await axios.post(`${BASE_URL}/singup`, data)
+    return response.data
+  },
+  login: async (data: UserProps) => {
+    const response = await axios.post(`${BASE_URL}/singin`, data)
+    return response.data
   },
 }
 
