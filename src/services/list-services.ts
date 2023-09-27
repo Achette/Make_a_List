835b1217f95e6a06c1407f3784e07cb5d7b38771
Achette <i.achette@live.com>
2023-09-27 17:01:12 -0300

    feat/pi3-38: cria services para listas

---
+++ src/services/list-services.ts
@@ -0,0 +1,13 @@
+import { AxiosRequestConfig } from 'axios'
+import { requestBackend } from './request'
+
+export const getLists = {
+  getAll: async () => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/purchase-list',
+    }
+
+    return requestBackend(config)
+  },
+}
