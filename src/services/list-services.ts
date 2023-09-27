e369503fa26a15438bcc4500e0f23e770de1e03b
Achette <i.achette@live.com>
2023-09-27 20:14:09 -0300

    feat/pi3-38: adiciona timeout para requisição

--- src/services/list-services.ts
+++ src/services/list-services.ts
@@ -1,11 +1,19 @@
 import { AxiosRequestConfig } from 'axios'
 import { requestBackend } from './request'
 
+function fetchWithTimeout(timeoutMs: number) {
+  const abortController = new AbortController()
+  setTimeout(() => abortController.abort(), timeoutMs || 0)
+
+  return abortController.signal
+}
+
 export const getLists = {
   getAll: async () => {
     const config: AxiosRequestConfig = {
       method: 'GET',
       url: '/purchase-list',
+      signal: fetchWithTimeout(5000),
     }
 
     return requestBackend(config)
