3b22d97d0e8147495ce299f601b6ab7b77ea85f5
Achette <i.achette@live.com>
2023-10-01 20:54:46 -0300

    perf/pi3-51: testes de comunicação front/back

--- src/constants/system.ts
+++ src/constants/system.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://make-a-list-back.onrender.com'
+export const BASE_URL = 'https://odd-lime-sockeye-hose.cyclic.app'
 
 export const TOKEN_KEY = 'com.dev.make_a_list/Token'
