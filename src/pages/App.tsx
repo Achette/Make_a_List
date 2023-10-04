248cce38fe3dafcb0386171832ff6d36ee6e95c7
Achette <i.achette@live.com>
2023-10-03 22:49:03 -0300

    feat/pi3-57: remove context

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SidebarContext, ListNameContext } from 'contexts'
+import { SidebarContext } from 'contexts'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import {
   Home,
@@ -15,28 +15,25 @@ import {
 
 function App() {
   const [collapse, setCollapse] = React.useState<boolean>(false)
-  const [listName, setListName] = React.useState<string>('')
 
   return (
-    <ListNameContext.Provider value={{ listName, setListName }}>
-      <SidebarContext.Provider value={{ collapse, setCollapse }}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/account" element={<AccessAccount />}>
-              <Route index element={<Login />} />
-              <Route path="signin" element={<SignIn />} />
-              <Route path="new-password" element={<NewPassword />} />
-            </Route>
-            <Route path="/lists" element={<Dashboard />}>
-              <Route index element={<Lists />} />
-              <Route path="/lists/:id" element={<ListDetail />} />
-              <Route path="/lists/new-list" element={<NewList />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </SidebarContext.Provider>
-    </ListNameContext.Provider>
+    <SidebarContext.Provider value={{ collapse, setCollapse }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/account" element={<AccessAccount />}>
+            <Route index element={<Login />} />
+            <Route path="signin" element={<SignIn />} />
+            <Route path="new-password" element={<NewPassword />} />
+          </Route>
+          <Route path="/lists" element={<Dashboard />}>
+            <Route index element={<Lists />} />
+            <Route path="/lists/:id" element={<ListDetail />} />
+            <Route path="/lists/new-list" element={<NewList />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </SidebarContext.Provider>
   )
 }
 
