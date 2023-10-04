b1405a3f112f0771b56d4d1aabfa17186d920420
Achette <i.achette@live.com>
2023-10-04 09:54:25 -0300

    feat/pi3-57: adiciona regra para interceptar requisi ção

--- src/services/list-services.ts
+++ src/services/list-services.ts
@@ -5,6 +5,7 @@ export const getAll = async () => {
   const config: AxiosRequestConfig = {
     method: 'GET',
     url: '/purchase-list',
+    signal: AbortSignal.timeout(5000),
   }
 
   return requestBackend(config)
@@ -14,6 +15,7 @@ export const getListById = async (id?: string) => {
   const config: AxiosRequestConfig = {
     method: 'GET',
     url: `/purchase-list/products?id=${id}`,
+    signal: AbortSignal.timeout(5000),
   }
 
   return requestBackend(config)
