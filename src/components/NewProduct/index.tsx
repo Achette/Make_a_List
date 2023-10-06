0aae0e8c40355276be1975dec479691e2bd49efa
Achette <i.achette@live.com>
2023-10-06 04:29:27 -0300

    feat/pi3-59: adiciona função para adicionar novos produtos na lista

--- src/components/NewProduct/index.tsx
+++ src/components/NewProduct/index.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React from 'react'
 import { useMedia } from 'hooks'
 import { CategorySelect } from 'components'
+import { addNewProduct } from 'services/list-services'
+import { useNavigate, useParams } from 'react-router-dom'
 import {
   Box,
   Flex,
@@ -11,73 +14,177 @@ import {
   ModalOverlay,
   ModalContent,
   ModalBody,
+  Button,
 } from '@chakra-ui/react'
 
-export const NewProduct = () => {
-  const { isOpen, onClose } = useDisclosure()
+type NewProductProps = {
+  modal: boolean
+  setModal: (arg: boolean) => void
+  fetchList: () => Promise<void>
+}
+
+type InputsProps = {
+  name: string
+  quantity: string
+  price: string
+  place: string
+}
+
+export const NewProduct = ({ modal, setModal, fetchList }: NewProductProps) => {
+  const { onClose, onOpen } = useDisclosure()
   const { isDesktop } = useMedia()
+  const { id } = useParams()
+  const navigate = useNavigate()
+
+  const [category, setCategory] = React.useState<string>('')
+  const [newProduct, setNewProduct] = React.useState<InputsProps>({
+    name: '',
+    place: '',
+    price: '',
+    quantity: '',
+  })
+
+  const handleGetNewProduct = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setNewProduct({ ...newProduct, [name]: value })
+  }
+
+  const handleGetCategory = (category: string) => {
+    setCategory(category)
+  }
+
+  const handleAddNewProduct = async () => {
+    try {
+      await addNewProduct(
+        id!,
+        newProduct.name,
+        Number(newProduct.quantity),
+        category,
+        Number(newProduct.price),
+        newProduct.place
+      )
+
+      fetchList()
+      setModal(false)
+      navigate(`/lists/${id}`)
+
+      setNewProduct({
+        name: '',
+        place: '',
+        price: '',
+        quantity: '',
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
   return (
-    <Modal onClose={onClose} size={isDesktop ? 'lg' : 'full'} isOpen={isOpen}>
+    <Modal onClose={onClose} size={isDesktop ? 'lg' : 'full'} isOpen={modal}>
       <ModalOverlay />
-      <ModalContent backgroundColor={'#ffff'}>
-        <ModalBody>
-          <Box padding="1rem">
+      <ModalContent backgroundColor="white">
+        <ModalBody border="1px solid teal">
+          <Flex flexDirection="column">
             <Flex
+              order={isDesktop ? 3 : 1}
+              my={isDesktop ? '2rem' : 'unset'}
               fontSize="1.0625rem"
               fontWeight={500}
               color="blue.900"
               justifyContent={'space-between'}
             >
-              <button type="button" onClick={onClose}>
+              <Button
+                type="button"
+                w={isDesktop ? '10rem' : 'fit-content'}
+                color="red.400"
+                fontSize="1.0625rem"
+                fontWeight={500}
+                lineHeight="1.375rem"
+                letterSpacing="-0.02563rem"
+                bg="none"
+                px="0"
+                onClick={() => setModal(false)}
+              >
                 Cancelar
-              </button>{' '}
-              <button type="button">Adicionar Produto</button>
+              </Button>
+              <Button
+                w={isDesktop ? '10rem' : 'fit-content'}
+                type="button"
+                color={isDesktop ? 'white' : 'blue.900'}
+                fontSize="1.0625rem"
+                fontWeight={500}
+                lineHeight="1.375rem"
+                letterSpacing="-0.02563rem"
+                bg={isDesktop ? 'blue.900' : 'none'}
+                px="0"
+                onClick={() => handleAddNewProduct()}
+              >
+                Adicionar
+              </Button>
             </Flex>
-            <Text color="blue.900" fontSize="2.125rem" fontWeight={700}>
-              Novo produto
-            </Text>
-            <Input
-              mt={5}
-              variant="flushed"
-              placeholder="Nome do produto"
-              color="blue.900"
-              maxWidth="25rem"
-              _placeholder={{ color: 'blue.900', opacity: '0.6' }}
-              required
-            />
-            <Input
-              mt={5}
-              variant="flushed"
-              placeholder="Local da compra"
-              color="blue.900"
-              maxWidth="25rem"
-              _placeholder={{ color: 'blue.900', opacity: '0.6' }}
-            />
-
-            <Flex>
+            <Box p="0.5rem" order={isDesktop ? 1 : 2}>
+              <Text
+                color="blue.900"
+                fontSize="2.125rem"
+                fontWeight={700}
+                onClick={onOpen}
+              >
+                Novo produto
+              </Text>
               <Input
+                name="name"
+                value={newProduct.name}
+                onChange={(e) => handleGetNewProduct(e)}
                 mt={5}
                 variant="flushed"
-                placeholder="Preço"
+                placeholder="Nome do produto*"
+                color="blue.900"
+                maxWidth="25rem"
+                outline="none"
+                _placeholder={{ color: 'blue.900', opacity: '0.6' }}
+                required
+              />
+              <Input
+                mt={5}
+                name="place"
+                value={newProduct.place}
+                onChange={(e) => handleGetNewProduct(e)}
+                variant="flushed"
+                placeholder="Local da compra"
                 color="blue.900"
                 maxWidth="25rem"
                 _placeholder={{ color: 'blue.900', opacity: '0.6' }}
               />
-              <Input
-                mt={5}
-                variant="flushed"
-                placeholder="Quantidade"
-                color="blue.900"
-                maxWidth="25rem"
-                _placeholder={{ color: 'blue.900', opacity: '0.6' }}
-              />
-            </Flex>
-          </Box>
-
-          <Box mt={5}>
-            <CategorySelect />
-          </Box>
+              <Flex>
+                <Input
+                  mt={5}
+                  type="number"
+                  name="price"
+                  value={newProduct.price}
+                  onChange={(e) => handleGetNewProduct(e)}
+                  variant="flushed"
+                  placeholder="Preço"
+                  color="blue.900"
+                  maxWidth="25rem"
+                  _placeholder={{ color: 'blue.900', opacity: '0.6' }}
+                />
+                <Input
+                  mt={5}
+                  name="quantity"
+                  value={newProduct.quantity}
+                  onChange={(e) => handleGetNewProduct(e)}
+                  variant="flushed"
+                  placeholder="Quantidade"
+                  color="blue.900"
+                  maxWidth="25rem"
+                  _placeholder={{ color: 'blue.900', opacity: '0.6' }}
+                />
+              </Flex>
+            </Box>
+            <Box mt={5} order={2}>
+              <CategorySelect getCategory={handleGetCategory} />
+            </Box>
+          </Flex>
         </ModalBody>
       </ModalContent>
     </Modal>
