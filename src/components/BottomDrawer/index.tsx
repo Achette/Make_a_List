c51d2fff376e1b933d10151cab1eee900b429dda
Igor Carvalho Achete <i.achette@live.com>
2023-10-09 10:14:18 -0300

    Refact(pi3-64): Refatora componente e adiciona função de excluir lista (#28)
    * ajusta configuração do lint
    
    * atualiza package-lock
    
    * feat/pi3-64: remove componente mobile e adiciona função de excluir lista
    
    * feat/pi3-64: remove componente desktop e adiciona função de excluir lista
    
    * style/pi3-64: ajusta px para rem
    
    * style/pi3-64: altera type de input e usa sintax sugar para parse number
    
    * feat/pi3-64: adiciona props ao componente
    
    * feat/pi3-64: cria service para excluir lista
    
    * feat/pi3-64: adiciona abort()

--- src/components/BottomDrawer/index.tsx
+++ src/components/BottomDrawer/index.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React from 'react'
-import { DrawerNavigation } from './iconsList'
 import { HiDotsVertical } from 'react-icons/hi'
+import { deleteList } from 'services/list-services'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import {
   Drawer,
   DrawerBody,
@@ -8,10 +10,30 @@ import {
   Flex,
   Icon,
   useDisclosure,
+  Link as LinkChakra,
+  Text,
 } from '@chakra-ui/react'
+import {
+  MdContentCopy,
+  MdDeleteOutline,
+  MdOutlineArchive,
+  MdOutlineBookmarkAdd,
+} from 'react-icons/md'
 
 export const BottomDrawer = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const navigate = useNavigate()
+  const { id } = useParams()
+
+  const deleteListById = async (id: string) => {
+    try {
+      await deleteList(id)
+      navigate(-1)
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
   return (
     <Flex>
@@ -21,6 +43,7 @@ export const BottomDrawer = () => {
         h="1.5rem"
         color="blue.900"
         onClick={onOpen}
+        cursor="pointer"
       />
       <Drawer
         isOpen={isOpen}
@@ -30,7 +53,99 @@ export const BottomDrawer = () => {
       >
         <DrawerContent>
           <DrawerBody>
-            <DrawerNavigation />
+            <>
+              <Flex
+                w="full"
+                alignItems="center"
+                m="1.25rem 0"
+                onClick={() => deleteListById(id!)}
+              >
+                <Icon
+                  as={MdDeleteOutline}
+                  w="2rem"
+                  h="1.5rem"
+                  color="gray.100"
+                />
+                <LinkChakra
+                  as={Link}
+                  _hover={{ textDecoration: 'none' }}
+                  _focus={{ border: 'none' }}
+                >
+                  <Text
+                    fontSize="1rem"
+                    color="gray.100"
+                    fontWeight={500}
+                    ml="1rem"
+                  >
+                    Excluir
+                  </Text>
+                </LinkChakra>
+              </Flex>
+
+              <Flex w="full" alignItems="center" m="1.25rem 0">
+                <Icon
+                  as={MdOutlineArchive}
+                  w="2rem"
+                  h="1.5rem"
+                  color="gray.100"
+                />
+                <LinkChakra
+                  as={Link}
+                  _hover={{ textDecoration: 'none' }}
+                  _focus={{ border: 'none' }}
+                >
+                  <Text
+                    fontSize="1rem"
+                    color="gray.100"
+                    fontWeight={500}
+                    ml="1rem"
+                  >
+                    Arquivar
+                  </Text>
+                </LinkChakra>
+              </Flex>
+
+              <Flex w="full" alignItems="center" m="1.25rem 0">
+                <Icon
+                  as={MdOutlineBookmarkAdd}
+                  w="2rem"
+                  h="1.5rem"
+                  color="gray.100"
+                />
+                <LinkChakra
+                  as={Link}
+                  _hover={{ textDecoration: 'none' }}
+                  _focus={{ border: 'none' }}
+                >
+                  <Text
+                    fontSize="1rem"
+                    color="gray.100"
+                    fontWeight={500}
+                    ml="1rem"
+                  >
+                    Salvar como modelo
+                  </Text>
+                </LinkChakra>
+              </Flex>
+
+              <Flex w="full" alignItems="center" m="1.25rem 0">
+                <Icon as={MdContentCopy} w="2rem" h="1.5rem" color="gray.100" />
+                <LinkChakra
+                  as={Link}
+                  _hover={{ textDecoration: 'none' }}
+                  _focus={{ border: 'none' }}
+                >
+                  <Text
+                    fontSize="1rem"
+                    color="gray.100"
+                    fontWeight={500}
+                    ml="1rem"
+                  >
+                    Criar cópia
+                  </Text>
+                </LinkChakra>
+              </Flex>
+            </>
           </DrawerBody>
         </DrawerContent>
       </Drawer>
