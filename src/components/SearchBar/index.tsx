b7b44a022d5378260586bbd5c0053c3bf4a321f6
Achette <i.achette@live.com>
2024-01-21 14:45:41 -0300

    resolve conflitos

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { getUser } from 'hooks'
 import { MdMenu } from 'react-icons/md'
 import { useSidebarCollapse } from 'contexts'
-import { useSidebarCollapse } from 'contexts'
 import { useNavigate } from 'react-router-dom'
 import { Avatar, Flex, HStack, IconButton, Input } from '@chakra-ui/react'
 
