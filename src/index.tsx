4aecb09e17e9c3645b1cf5eb1a08126895235574
Achette <i.achette@live.com>
2024-01-23 10:24:40 -0300

    refactor: move SidebarProvider para padronizar o projeto

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import { theme } from 'theme/theme'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom/client'
+import { SidebarProvider } from 'contexts'
 import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ChakraProvider theme={theme}>
-        <App />
-      </ChakraProvider>
+      <SidebarProvider>
+        <ChakraProvider theme={theme}>
+          <App />
+        </ChakraProvider>
+      </SidebarProvider>
     </Provider>
   </React.StrictMode>
 )
