c9d5f9ef50c4a1a08f4c3f4d16f74dd8607e3067
Achette <i.achette@live.com>
2024-03-01 19:10:42 -0300

    feat: adiciona provider no root da aplicação

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,20 @@ import { theme } from 'theme/theme'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom/client'
-import { SidebarProvider } from 'contexts'
 import { ChakraProvider } from '@chakra-ui/react'
+import { LoadingProvider, SidebarProvider } from 'contexts'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <SidebarProvider>
-        <ChakraProvider theme={theme}>
-          <App />
-        </ChakraProvider>
-      </SidebarProvider>
+      <LoadingProvider>
+        <SidebarProvider>
+          <ChakraProvider theme={theme}>
+            <App />
+          </ChakraProvider>
+        </SidebarProvider>
+      </LoadingProvider>
     </Provider>
   </React.StrictMode>
 )
