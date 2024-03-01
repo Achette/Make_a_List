8a7c073e43909020955cf8e4e81110704518f8af
Achette <i.achette@live.com>
2024-03-01 19:12:10 -0300

    feat: cria componente Skeletons

---
+++ src/components/Skeleton/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Skeleton, SkeletonText } from '@chakra-ui/react'
+
+export const ListSkeletons = () => {
+  return (
+    <>
+      <Skeleton mt="1rem" height="12" width="full" />
+      <Skeleton mt="1rem" height="12" width="full" />
+      <Skeleton mt="1rem" height="12" width="full" />
+      <Skeleton mt="1rem" height="12" width="full" />
+      <Skeleton mt="1rem" height="12" width="full" />
+      <Skeleton mt="1rem" height="12" width="full" />
+    </>
+  )
+}
+
+export const TextSkeleton = () => {
+  return <SkeletonText noOfLines={1} skeletonHeight="10" width="16rem" />
+}
